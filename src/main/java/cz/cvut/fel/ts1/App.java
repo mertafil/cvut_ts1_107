6da4602c1555314d5cabe0920337e41f997b3a68
Filip Merta <mertafil@fel.cvut.cz>
2023-03-09 15:27:13 +0100

    factorial method added

--- src/main/java/cz/cvut/fel/ts1/App.java
+++ src/main/java/cz/cvut/fel/ts1/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // System.out.println( "Hello World!" );
+    }
+
+    public int factorial(int num) {
+        int result = 1;
+        int i = 1;
+
+        while (i <= num) {
+            result *= i;
+            i++;
+        }
+
+        return result;
     }
 }
