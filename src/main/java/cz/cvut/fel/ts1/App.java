f42c32c87ff3b596de52e0d943992b6aaf112d17
Filip Merta <mertafil@fel.cvut.cz>
2023-03-09 15:22:39 +0100

    init java maven project

---
+++ src/main/java/cz/cvut/fel/ts1/App.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.ts1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
